1d0c4eb30b8f95f50c37ed2e9419ce25c4794482
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-06-12 12:23:34 -0400

    Add button with icon
    This is for checking that icons are properly installed.

--- lib/App/Example.pm
+++ lib/App/Example.pm
@@ -3,6 +3,7 @@ package App::Example;
 
 use Mu;
 use Gtk3 -init;
+use Glib;
 
 =attr app_name
 
@@ -21,10 +22,48 @@ lazy main_window => sub {
 	my $w = Gtk3::Window->new;
 	$w->set_title( $self->app_name );
 
+	$w->add(my $box = Gtk3::Box->new('vertical', 0));
+
+	$box->pack_start( $self->clicking_button, Glib::TRUE, Glib::FALSE, 0 );
+
 	$w->signal_connect(
 		destroy => \&on_application_quit_cb, $self );
 	$w->set_default_size( 800, 600 );
 	$w;
+};
+
+=attr clicking_button
+
+A button for clicking
+
+=cut
+lazy clicking_button => sub {
+	my ($self) = @_;
+
+	# NOTE Getting icon size out of enum. Look for Gtk3::IconSize overrides
+	# in Gtk3.pm
+	my $button = Gtk3::Button->new_from_icon_name('input-mouse',
+		Glib::Object::Introspection->convert_sv_to_enum( 'Gtk3::IconSize', 'button' )
+	);
+	$button->set_label("Click here");
+	$button->set('always-show-image', Glib::TRUE);
+	my $count = 0;
+	my $label = "Click here";
+
+	$button->signal_connect(
+		clicked => sub {
+			++$count;
+			$button->set_label(
+				sprintf(
+					( $count == 1
+						? "You have clicked %d time!"
+						: "You have clicked %d times!" ),
+					$count )
+				);
+		}
+	);
+
+	$button;
 };
 
 =method main
