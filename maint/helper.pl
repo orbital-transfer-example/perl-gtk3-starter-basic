c98e806d15c8cf8c484f5d45bdb4404e8805b89a
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-07-05 15:42:38 -0400

    Remove sandbox

--- maint/helper.pl
+++ maint/helper.pl
@@ -1483,7 +1483,7 @@ sub cmd_build_dmg {
 	IPC::Cmd::run( command => [
 		$create_dmg,
 		qw(--hdiutil-verbose),
-		qw(--sandbox-safe),
+		#qw(--sandbox-safe),
 		qw(--volname), $volume_name,
 		qw(--window-size 550 500),
 		qw(--icon-size 48),
